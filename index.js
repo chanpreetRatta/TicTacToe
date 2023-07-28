aba4347134f3155f22e9cd32a06c6b6034a18129
Devolpment <devolpment@MacBook-Pro-2.local>
2023-07-27 18:52:35 -0700

    implement the board, render the input onCLick, toggle the input after click

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const playBox = document.querySelectorAll(".play-box");
+const board = document.querySelectorAll(".play-box");
 
-function thisFunction(e) {
-  let array = [];
-  e.target.textContent = "X";
+const player = (name, mark) => {
+  return { name, mark };
+};
+
+const GamingBoard = (() => {
+  const player1 = player("Chanpeet", "X");
+  const player2 = player("Singh", "0");
+  let turn = currentPlayer();
+
+  const renderOnTheDisplay = (boxNumber) => {
+    board[boxNumber].appendChild(document.createTextNode(turn().mark));
+  };
+
+  // toggle between the current players
+  function currentPlayer() {
+    let current = player2;
+    return function () {
+      current = current === player2 ? player1 : player2;
+      return current;
+    };
+  }
+  return { renderOnTheDisplay, currentPlayer };
+})();
+
+// function to get the target box on the board through onClick event
+function getBoardInput(event) {
+  let targetBox = event.target.dataset.box;
+
+  //make sure only one entry in one box
+  if (board[targetBox].childNodes.length === 0) {
+    GamingBoard.renderOnTheDisplay(targetBox);
+  }
 }
 
-playBox.forEach((item) => {
-  item.addEventListener("click", thisFunction);
-});
+board.forEach((element) => element.addEventListener("click", getBoardInput));
